1a72f792878092e9a4aec5893f47c6c09a4d898d
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-22 21:21:00 +0300

    Цикл for и цикл в цикле

---
+++ Introduction to programming languages/Lecture3/Cicles/Program.cs
@@ -0,0 +1,19 @@
+﻿//Цикл for
+string Method4(int count, string text){
+    string result = String.Empty;
+    for(int i = 0; i < count; i++){
+        result = result + text;
+    }
+    return result;
+}
+// string res = Method4(10, "йцукен ");
+// Console.WriteLine(res);
+
+//Цикл в цикле. Задача вывода таблицы умножения на экран.
+for (int i = 2; i <=10; i++){
+    for(int j = 2; j<=10; j++){
+        System.Console.WriteLine($"{i} x {j} = " +  i*j);//Использование интерполяции строк
+    }
+    System.Console.WriteLine();
+}
+
