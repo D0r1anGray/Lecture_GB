3250b1220d44f458fb8ae98a856734aa982021a5
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-27 16:11:21 +0300

    Сортировка выбором. От наибольшего к меньшему

---
+++ Introduction to programming languages/Lecture3/Task2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Отсортировать массив, используя сортировку Выбором. От наибольшего к меньшему.
+
+int[] arr = {2,3,5,6,7,8,45,3,2,5,6,8,9,5,43,54,56,7,6};
+
+void PrintArray(int[] array){
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++){
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+//PrintArray(arr);
+
+void SelectionSort(int[] array){
+
+    for(int i = 0; i < array.Length - 1; i++){
+        int minPosition = i;
+
+        for(int j = i+1; j < array.Length; j++){
+            if(array[j] < array[minPosition]){
+                minPosition = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+        // array[i] = array[i] + array[minPosition];
+        // array[minPosition] = array[i] - array[minPosition];
+        // array[i] = array[i] - array[minPosition];
+        
+    }
+    
+}
+
+SelectionSort(arr);
+PrintArray(arr);
