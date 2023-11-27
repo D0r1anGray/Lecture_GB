5876f1b72577b1a7707ad15363c582de11f1a50e
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-27 17:59:07 +0300

    Многомерный массив. Раскарска. Факториал через рекурсию

---
+++ Introduction to programming languages/Lecture4/2dimensional_array/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+// string [,] table = new string [2,5];
+
+// table[1,2] = "Слово";
+
+// for (int rows = 0; rows < 2; rows++){
+//     for(int colums = 0; colums < 5; colums++){
+//         System.Console.WriteLine($"{table[rows,colums]}--");
+//     }
+// }
+
+
+
+void PrintArray(int[,] matr){
+
+    for(int i = 0; i < matr.GetLength(0); i++){
+        for(int j = 0; j < matr.GetLength(1); j++){
+            System.Console.Write($"{matr[i,j]} ");
+        }
+    System.Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr){
+
+    for(int i = 0; i < matr.GetLength(0); i++){
+        for(int j = 0; j < matr.GetLength(1); j++){
+            matr[i,j] = new Random().Next(1,100);
+            System.Console.Write($"{matr[i,j]} ");
+        }
+    System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+System.Console.WriteLine();
+FillArray(matrix);
+
+
---
+++ Introduction to programming languages/Lecture4/Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿//Факториал через рекурсию
+
+double Factorial(double n){
+    if(n == 1 || n == 0){
+        return 1;
+    }
+    else{
+        return n * Factorial(n - 1);
+    }
+}
+
+// System.Console.WriteLine(Factorial(0));
+
+for(int i = 0; i < 40; i++){
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}
