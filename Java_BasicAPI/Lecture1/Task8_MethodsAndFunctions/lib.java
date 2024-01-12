3521eade799d128ce9241225bea890d8b262e9de
Dorian Gray <d0r1an.gray@yandex.ru>
2024-01-12 19:22:13 +0300

    Task8_MethodsAndFunctions. Методы и функции

---
+++ Java_BasicAPI/Lecture1/Task8_MethodsAndFunctions/lib.java
@@ -0,0 +1,9 @@
+package Java_BasicAPI.Lecture1.Task8_MethodsAndFunctions;
+
+public class lib {
+    static void sayHi(){
+        System.out.println("hi");
+    }
+
+    
+}
---
+++ Java_BasicAPI/Lecture1/Task8_MethodsAndFunctions/program.java
@@ -0,0 +1,12 @@
+package Java_BasicAPI.Lecture1.Task8_MethodsAndFunctions;
+
+public class program {
+    public static void main(String[] args) {
+        lib.sayHi();
+    }
+
+    // static void sayHi(){
+    //     System.out.println("hi");
+    // }
+    
+}
